076bc05e56656b0044d307f29f1fe804efe45b4a
Rafał Grzejszczyk <rafal@grzejszczyk.pl>
2020-06-09 14:56:30 +0200

    Zima 2019/2020

---
+++ json-ld.js
@@ -0,0 +1,52 @@
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org/",
+  "@type": "Service",
+  "serviceType": "Opieka nad dziećmi",
+	"description": "Zajęcia w sali zabaw, wyjazdy, wyjścia na miasto. Od pn. do pt. w godz. 8-16 lub 9-17. Dla dzieci 5-12 lat.",
+  "image": "http://polkolonie.askalandia.pl/images/wakacje-w-krainie-zabaw.jpg",
+  "provider": {
+    "@type": "ChildCare",
+    "name": "Askalandia - Kraina Zabaw",
+    "image": "http://www.askalandia.pl/images/logo_bg.jpg",
+    "address": {
+      "@type": "PostalAddress",
+      "addressLocality": "Gorzów Wielkopolski",
+      "postalCode": "66-400",
+      "streetAddress": "Konstytucji 3 Maja 102"
+    },
+    "priceRange": "$$$",
+    "telephone": "+48 957830300",
+    "url": "http://www.askalandia.pl"
+  },
+  "areaServed": {
+    "@type": "City",
+    "name": "Gorzów Wielkopolski",
+    "@id": "https://pl.wikipedia.org/wiki/Gorz%C3%B3w_Wielkopolski"
+  },
+  "hasOfferCatalog": {
+    "@type": "OfferCatalog",
+    "name": "Półkolonie dla dzieci",
+    "itemListElement": [
+      {
+        "@type": "Offer",
+        "itemOffered": {
+          "@type": "Service",
+          "name": "Półkolonie letnie",
+          "description": "Zajęcia w czasie wakacji letnich",
+          "url": "http://polkolonie.askalandia.pl"
+        }
+      },
+      {
+        "@type": "Offer",
+        "itemOffered": {
+          "@type": "Service",
+          "name": "Półkolonie zimowe",
+          "description": "Zajęcia w czasie ferii zimowych",
+          "url": "http://polkolonie.askalandia.pl"
+        }
+      }
+    ]
+  }
+}
+</script>
